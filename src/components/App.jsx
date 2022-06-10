087d7ba697658e5c37439c83ae4ead9e7e7df4f2
xcsnowcity <1094553181xxcc@gmail.com>
2022-06-10 19:44:50 +0800

    restart

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
-import Button from "@mui/material/Button";
+import {Button} from "@mui/material";
 
 const App = () => {
   return (
