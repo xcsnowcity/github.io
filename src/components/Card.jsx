ee8881602236871be3309f8e3077ea1fe85b1c9d
xcsnowcity <1094553181xxcc@gmail.com>
2022-06-18 14:20:20 +0800

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ export default function Card(props) {
     <div className="wrapper w-full md:w-1/2 h-fit mb-10 px-2 md:px-5 ">
       <a href={props.href} className="tex">
       <div >
-      <img src={props.img} alt="" loading="eager" className="w-full h-full object-cover rounded-full hover:scale-95 transition-transform" />
+      <img src={props.img} alt="" loading="lazy" className="w-full h-full object-cover rounded-full hover:scale-95 transition-transform" />
       </div>
       </a>
       <h2 className="text-center mt-1 text-xl font-semibold font-serif">{props.name}</h2>
