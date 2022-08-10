bb2c06d021c39e81955f07138d3ed4203d82b6b0
xcsnowcity <1094553181xxcc@gmail.com>
2022-08-10 08:49:54 +0800

    Fix Cumulative Layout Shift (CLS) Issues

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,13 @@
 export default function Card(props) {
   return (
-    <div className="wrapper w-full md:w-1/2 h-fit mb-10 px-2 md:px-5 ">
-      <a href={props.href} className="tex">
+    <div className="wrapper w-full md:w-1/2 mb-10 px-2 md:px-5 ">
+      <a href={props.href} className="tex" >
       <div >
-      <img src={props.img} alt="" loading="lazy" className="w-full h-full object-cover rounded-full hover:scale-95 transition-transform" />
+      <img src={props.img} alt="" loading="eager" className="w-full aspect-video object-cover rounded-full hover:scale-95 transition-transform" />
       </div>
-      </a>
       <h2 className="text-center mt-1 text-xl font-semibold font-serif">{props.name}</h2>
+      </a>
+      
       
     </div>
   );
